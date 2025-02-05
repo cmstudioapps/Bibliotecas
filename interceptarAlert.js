4e61ed79d05b451ce90c85364298c79757583020
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-02-05 18:36:09 -0300

    Atualizar o interceptarAlert.js

--- interceptarAlert.js
+++ interceptarAlert.js
@@ -45,6 +45,7 @@
       width: 100%;
       height: 100%;
       background: rgba(0, 0, 0, 0.5);
+      color: black;
       display: flex;
       align-items: center;
       justify-content: center;
